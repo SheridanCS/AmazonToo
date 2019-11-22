81301ec33e43ee398dbc6d8927b56b846c645a61
Timothy Catibog <catibogt@sheridancollege.ca>
2019-11-22 16:07:52 -0500

    Initial commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AmazonToo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
