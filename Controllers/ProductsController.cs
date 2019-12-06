931cb926c827768c45c90f962390ec8f9fbfa691
Kell <kellenevoy@gmail.com>
2019-12-06 16:40:58 -0500

    Minor tweaks to View

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,17 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using AmazonToo.Models;
 
 namespace AmazonToo.Controllers
 {
     public class ProductsController : Controller
     {
+        private AmazonDbContext _context;
+
+        public ProductsController(AmazonDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            
             return View();
         }
 
         public IActionResult Details(string productId)
         {
-            return View();
+            Product p = _context.Products.Find(productId);
+            if (p == null)
+            {
+                return View("NotFound");
+            }
+            return View(p);
         }
     }
 }
